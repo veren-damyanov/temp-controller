e31376cd10d0b71d6aaa7fd11de63168c1435358
Yassen Damyanov <yd@itlabs.bg>
2022-06-07 20:29:41 +0300

    Make file reading truly async

--- src/app.py
+++ src/app.py
@@ -1,28 +1,29 @@
 import asyncio
-import json
 from os import path
 
+import aiofiles
 from sanic import Sanic
 from sanic import response
 
 from config import SENSORS, SENSORS_ROOT
 
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic('temp-controller')
 
 
 async def read_temp(sensor_name):
-    sensor_id = SENSORS.get(sensor_name)
-    data = {'sensor-id': sensor_id, 'sensor-name': sensor_name, 'raw-temp': None, 'adjusted-temp': None}
-    with open(path.join(SENSORS_ROOT, sensor_id, 'temperature')) as flh:
-        data['raw-temp'] = int(flh.read().strip())
-    return data
+    sensor_id = SENSORS[sensor_name]
+    async with aiofiles.open(path.join(SENSORS_ROOT, sensor_id, 'temperature')) as fhl:
+        raw_temp = int((await fhl.read()).strip())
+    return {
+        'sensor-id': sensor_id,
+        'sensor-name': sensor_name,
+        'raw-temp': raw_temp,
+        'adjusted-temp': None,
+    }
 
 
-@app.get("/temperature")
+@app.get('/temperature')
 async def get_temperature(request):
-    final_data = {'sensor_data': []}
-    results = await asyncio.gather(*(read_temp(sensor_id) for sensor_id in SENSORS.keys()))
-    for result in results:
-        final_data['sensor_data'].append(result)
-    return response.json(final_data)
+    sensor_data = await asyncio.gather(*(read_temp(sensor_id) for sensor_id in SENSORS.keys()))
+    return response.json({'sensor-data': sensor_data})
